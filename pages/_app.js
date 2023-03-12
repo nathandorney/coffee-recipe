a159901925f0d3e5ad342909494c449757e3c25e
Nathan <hello@nathandorney.co.uk>
2023-03-12 16:56:19 +0000

    font fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { Inter } from "next/font/google";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const inter = Inter({ subsets: ["latin"] });
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <div className={inter.className}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
