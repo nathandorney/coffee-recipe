c1e17ccf2c2cad14fcb0242aa1aa7b27b0fae7bb
Nathan <hello@nathandorney.co.uk>
2023-03-13 12:20:17 +0000

    back button added

--- pages/how-to-brew-chemex.js
+++ pages/how-to-brew-chemex.js
@@ -1,10 +1,16 @@
 import Chemex from "../components/Chemex";
 import Footer from "../components/Footer";
+import { ArrowLeft } from "phosphor-react";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <div className="layout">
+        <Link href="/" className="back">
+          <ArrowLeft size={16} weight="regular" />
+          Back to all recipes
+        </Link>
         <h1>Chemex Recipe</h1>
         <Chemex />
         <Footer />
--- pages/how-to-brew-v60.js
+++ pages/how-to-brew-v60.js
@@ -1,10 +1,16 @@
 import V60Guide from "../components/V60Guide";
 import Footer from "../components/Footer";
+import { ArrowLeft } from "phosphor-react";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <div className="layout">
+        <Link href="/" className="back">
+          <ArrowLeft size={16} weight="regular" />
+          Back to all recipes
+        </Link>
         <h1>V60 Recipe</h1>
         <V60Guide />
         <Footer />
